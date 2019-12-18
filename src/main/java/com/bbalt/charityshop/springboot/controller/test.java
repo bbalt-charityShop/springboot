b4da30f68c6e47dbc724bd8df959f65a45a77cba
linzhiwei <354326474@qq.com>
2019-12-18 23:41:53 +0800

    Swagger

---
+++ src/main/java/com/bbalt/charityshop/springboot/controller/test.java
@@ -0,0 +1,15 @@
+package com.bbalt.charityshop.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class test {
+    @RequestMapping("test")
+    @ResponseBody
+    public String test(){
+        return "test";
+    }
+}
